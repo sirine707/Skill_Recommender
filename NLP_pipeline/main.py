513c11d2539c82173ffab986885d644e1ac27330
sirine707 <Sirine.JNAYEH@esprit.tn>
2024-12-13 21:22:27 +0100

    refactored pipeline.py

--- NLP_pipeline/main.py
+++ NLP_pipeline/main.py
@@ -3,6 +3,12 @@ from src.pipeline import run_pipeline
 # List of file paths
 file_paths = ['data/licence.csv', 'data/masters.csv', 'scrapped_content_ref/file3.csv']
 output_path = 'output/combined_vectors.csv'
-
 # Run the pipeline
-run_pipeline(file_paths, output_path)
+similarities = run_pipeline(file_paths, output_path,'PCA',2)
+
+# Print the similarities
+print("Similarities between files:")
+for key, value in similarities.items():
+    print(f"{key}:")
+    for metric, metric_value in value.items():
+        print(f"  {metric}: {metric_value}")
